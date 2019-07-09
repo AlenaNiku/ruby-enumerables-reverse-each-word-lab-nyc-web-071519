b7f81343a05506a75ea67bc9963829125395febb
Alena Nikulina <alenanikulina0@gmail.com>
2019-07-09 22:25:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word('Hello there, and how are you?')
+def reverse_each_word("Hello there, and how are you?")
   my_array = "Hello there, and how are you?".split
   str = " "
   my_array.reverse {|word| str += "#{word}"}
